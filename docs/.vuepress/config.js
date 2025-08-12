85f1f0866bee6525d33bb96b1eb9f8e16c662714
melbraimah <mbraimah@54gradsoftware.de>
2025-08-12 13:19:11 +0200

    Top-Bar angelegt

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,30 @@ export default defineUserConfig({
   theme: defaultTheme({
     logo: 'DSG%20Logo%20Dokumentaion.png',
 
-    navbar: ['/'],
+    navbar: [
+  { text: 'Handbuch', link: '/get-started' },
+  { text: 'Kalenderfunktionen', link: '/DSG%20Funktionen/' },
+  {
+    text: 'Terminmanagement',
+    children: [
+      { text: 'Organisationsverwaltung', link: '/Terminverwaltung/Organisation/' },
+      { text: 'Einzeltermine', link: '/Terminverwaltung/Einzeltermine/' },
+      { text: 'Serientermine', link: '/Terminverwaltung/Serientermine/' },
+      { text: 'iCal-Dateien', link: '/Terminverwaltung/iCal-Dateien/' },
+      { text: 'Kalenderintegration', link: '/Terminverwaltung/Kalenderintegration/' },
+      { text: 'Werbemittelgenerator', link: '/Werbemittelgenerator/' }
+    ]
+  },
+  {
+    text: 'Entwicklungsbereich',
+    children: [
+      { text: 'Hosting', link: '/Entwicklungsbereich/Hosting/' },
+      { text: 'Repositories', link: '/Entwicklungsbereich/Repositories/' },
+      { text: 'Technologien', link: '/Entwicklungsbereich/Technologien/' }
+    ]
+  }
+],
+
   }),
 
   bundler: viteBundler(),
